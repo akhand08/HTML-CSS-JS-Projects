c7176f653d4a5bc761efefe40f36bb36743a5eae
akhand08 <adib08@outlook.com>
2023-12-03 08:36:58 -0300

    Functionalities are also added. Only Start and Stop for now

--- stopwatch/script.js
+++ stopwatch/script.js
@@ -1,6 +1,71 @@
 
 
 
+// let secondCount = 0;
+// let minuteCount = 0;
+// let hourCount = 0;
+
+// let secondElement = document.getElementById("second");
+// let minuteElement = document.getElementById("minute");
+// let hourElement = document.getElementById("hour");
+// let intervalId;
+
+
+// function setValue() {
+//     secondElement.innerHTML = toString(secondCount).padStart(2,"0");
+//     minuteElement.innerHTML = toString(minuteCount).padStart(2,"0");
+//     hourElement.innerHTML = toString(hourCount).padStart(2, "0");
+// }
+
+// function StartCounting() {
+
+//     setValue();
+
+//     intervalId = setInterval(() => {
+//         if( secondCount === 59) {
+//             secondCount = 0;
+            
+//             if(minuteCount == 59) {
+//                 minuteCount = 0;
+//                 hourCount ++;
+//             }
+//             else {
+//                 minuteCount++;
+//             }
+
+//         }
+//         else {
+//             secondCount++;
+//         }
+
+//         setValue();
+
+        
+//     }, 900);
+
+    
+// }
+
+// document.querySelector("#start").addEventListener("click", event => {
+//     event.preventDefault();
+//     StartCounting();
+// })
+
+
+// document.querySelector("#stop").addEventListener("click", event => {
+//     event.preventDefault();
+//     clearInterval(intervalId);
+
+// })
+
+
+
+
+
+
+
+
+
 let secondCount = 0;
 let minuteCount = 0;
 let hourCount = 0;
@@ -8,52 +73,45 @@ let hourCount = 0;
 let secondElement = document.getElementById("second");
 let minuteElement = document.getElementById("minute");
 let hourElement = document.getElementById("hour");
-let intervalId;
-
+let intervalId; // Declare intervalId globally
 
 function setValue() {
-    secondElement.innerHTML = toString(secondCount).padStart(2,"0");
-    minuteElement.innerHTML = toString(minuteCount).padStart(2,"0");
-    hourElement.innerHTML = toString(hourCount).padStart(2, "0");
+    secondElement.innerHTML = secondCount.toString().padStart(2, "0");
+    minuteElement.innerHTML = minuteCount.toString().padStart(2, "0");
+    hourElement.innerHTML = hourCount.toString().padStart(2, "0");
 }
 
-function StartCounting() {
-
+function startCounting() {
     setValue();
 
     intervalId = setInterval(() => {
-        if( secondCount === 59) {
+        if (secondCount === 59) {
             secondCount = 0;
-            
-            if(minuteCount == 59) {
+
+            if (minuteCount === 59) {
                 minuteCount = 0;
-                hourCount ++;
-            }
-            else {
+                hourCount++;
+            } else {
                 minuteCount++;
             }
-
-        }
-        else {
+        } else {
             secondCount++;
         }
 
         setValue();
-
-        
     }, 900);
-
-    
 }
 
-document.querySelector("#start").addEventListener("click", event => {
-    event.preventDefault();
-    StartCounting();
-})
-
-
-document.querySelector("#stop").addEventListener("click", event => {
-    event.preventDefault();
+function stopCounting() {
     clearInterval(intervalId);
+}
 
-})
+document.querySelector("#start").addEventListener("click", (event) => {
+    event.preventDefault();
+    startCounting();
+});
+
+document.querySelector("#stop").addEventListener("click", (event) => {
+    event.preventDefault();
+    stopCounting();
+});
